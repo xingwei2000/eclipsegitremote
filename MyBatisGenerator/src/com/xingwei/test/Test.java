7c1c340074c26fd755a08555157b83aa5f5dee57
XW <58770348+xingwei2000@users.noreply.github.com>
2020-09-12 21:58:50 +0800

    Update Test.java

--- MyBatisGenerator/src/com/xingwei/test/Test.java
+++ MyBatisGenerator/src/com/xingwei/test/Test.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+//閭紵
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
@@ -16,15 +16,15 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 public class Test {
 	public static void main(String[] args) throws InvalidConfigurationException, IOException, XMLParserException, SQLException, InterruptedException {
 		
-		File file = new File("src/generator.xml");  //配置文件
-		List<String> warnings = new ArrayList<String>();  //警告信息
+		File file = new File("src/generator.xml");  //閰嶇疆鏂囦欢
+		List<String> warnings = new ArrayList<String>();  //璀﹀憡淇℃伅
 		
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		
 		Configuration config = cp.parseConfiguration(file);
 		
 		DefaultShellCallback callBack = new DefaultShellCallback(true);
-		//逆向工程的核心类
+		//閫嗗悜宸ョ▼鐨勬牳蹇冪被
 		MyBatisGenerator generator = new MyBatisGenerator(config,callBack,warnings);
 		generator.generate(null);
 	}
